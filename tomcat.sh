64eb27366429b1080852ec37a10d97032b8233bc
pushpak167890 <69355892+pushpak167890@users.noreply.github.com>
2020-08-08 12:37:09 +0530

    tomcat.sh
    it is tomcat installation script for ubuntu 18.04

---
+++ tomcat.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#Install Java
+
+sudo apt update
+sudo apt install default-jdk
+
+
+#Creating directory
+
+sudo mkdir -p /opt/tomcat
+
+#Creating tomcat user and group
+
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+
+#Download and Install Tomcat 9 on Ubuntu 18.04
+
+sudo apt install unzip wget
+cd /tmp
+unzip apache-tomcat-*.zip
+sudo mv apache-tomcat-9.0.33/* /opt/tomcat/
+sudo ln -s /opt/tomcat /opt/tomcat/latest
+sudo chown -R tomcat: /opt/tomcat
+sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
+
+#Configure Environment variables
+
+echo "export CATALINA_HOME="/opt/tomcat"" >> ~/.bashrc
+source ~/.bashrc
+
+#Create Systemd unit file
+
+sudo nano /etc/systemd/system/tomcat.service
+
+[Unit]
+Description=Tomcat 9 servlet container
+After=network.target
+
+[Service]
+Type=forking
+User=tomcat
+Group=tomcat
+Environment="JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+Environment="CATALINA_BASE=/opt/tomcat/latest"
+Environment="CATALINA_HOME=/opt/tomcat/latest"
+Environment="CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+ExecStart=/opt/tomcat/latest/bin/startup.sh
+ExecStop=/opt/tomcat/latest/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl status tomcat
+sudo systemctl enable tomcat
+
+#Adjust the Firewall
+
+sudo ufw allow 8080/tcp

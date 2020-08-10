c4853bea0507ac9c40e6df5d52c21da8919364b4
pushpak167890 <69355892+pushpak167890@users.noreply.github.com>
2020-08-10 14:46:53 +0530

    Update tomcat9ver.sh

--- tomcat9ver.sh
+++ tomcat9ver.sh
@@ -41,6 +41,10 @@ source ~/.bashrc
 
 #Create Systemd unit file
 
+cd /etc/systemd/system
+sudo touch tomcat.service
+sudo chmod 777 tomcat.service
+
 sudo echo "[Unit]
 Description=Tomcat 9 servlet container
 After=network.target
@@ -48,16 +52,16 @@ After=network.target
 Type=forking
 User=tomcat
 Group=tomcat
-Environment="JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64"
-Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
-Environment="CATALINA_BASE=/opt/tomcat/latest"
-Environment="CATALINA_HOME=/opt/tomcat/latest"
-Environment="CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid"
-Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+Environment='JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64'
+Environment='JAVA_OPTS=-Djava.security.egd=file:///dev/urandom'
+Environment='CATALINA_BASE=/opt/tomcat/latest'
+Environment='CATALINA_HOME=/opt/tomcat/latest'
+Environment='CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid'
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
 ExecStart=/opt/tomcat/latest/bin/startup.sh
 ExecStop=/opt/tomcat/latest/bin/shutdown.sh
 [Install]
-WantedBy=multi-user.target > /etc/systemd/system/tomcat.service
+WantedBy=multi-user.target" > /etc/systemd/system/tomcat.service
 
 sudo systemctl daemon-reload
 
